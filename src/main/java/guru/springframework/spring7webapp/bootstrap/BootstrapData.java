9bb2fcc1cdb6467bcb100eb643c0cf36310878c2
slawe <slawe@minint>
2026-05-24 07:41:41 +0200

    add BootstrapData

---
+++ src/main/java/guru/springframework/spring7webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring7webapp.bootstrap;
+
+import org.springframework.stereotype.Component;
+
+import guru.springframework.spring7webapp.domain.Author;
+import guru.springframework.spring7webapp.domain.Book;
+import guru.springframework.spring7webapp.repositories.AuthorRepository;
+import guru.springframework.spring7webapp.repositories.BookRepository;
+
+@Component
+public class BootstrapData implements org.springframework.boot.CommandLineRunner {
+
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
+
+	public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Author eric = new Author();
+		eric.setFirstName("Eric");
+		eric.setLastName("Evans");
+
+		Book ddd = new Book();
+		ddd.setTitle("Domain Driven Design");
+		ddd.setIsbn("123456");
+
+		Author ericSaved = authorRepository.save(eric);
+		Book dddSaved = bookRepository.save(ddd);
+
+		Author rod = new Author();
+		rod.setFirstName("Rod");
+		rod.setLastName("Johnson");
+
+		Book noEJB = new Book();
+		noEJB.setTitle("J2EE Development without EJB");
+		noEJB.setIsbn("54757585");
+
+		Author rodSaved = authorRepository.save(rod);
+		Book noEJBSaved = bookRepository.save(noEJB);
+
+		ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+	}
+}
